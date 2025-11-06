cc2592fcb8301d1a2c466beff9d2f2adf74eecad
git-salmanshaikh <salmanchandsahabshaikh@gmail.com>
2025-11-06 11:51:25 +0530

    Added Lesson1

---
+++ FullStackPath/Javascript/lesson1/index.js
@@ -0,0 +1,35 @@
+//document.getElementById("count-el").innerText = 10;
+
+// let firstBatch = 5
+// let secondBatch = 7
+
+// let count = firstBatch
+
+// console.log(count)
+
+// *** learing how to use console.log and use multiple variables as well as adding them**
+// let myAge = 27
+// let humanDogRatio = 7
+
+// let myDogAge = myAge * humanDogRatio
+
+// console.log(myDogAge)
+
+
+//* another example */
+// let bonusPoints = 50
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
+
+
+function increment() {
+    console.log("The button was clicked")
+}
